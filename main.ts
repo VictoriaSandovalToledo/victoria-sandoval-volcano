a7dbdbe88cd51ea91a355156ea37d3b44fec3e45
VictoriaSandovalToledo <110991718+VictoriaSandovalToledo@users.noreply.github.com>
2022-09-07 21:51:00 -0700

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,70 @@
-basic.forever(function () {
-	
-})
+// Magma rising through a vent.
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+}
+// Lava erupting.
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+}
